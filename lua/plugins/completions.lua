b7715273311b59d253690770106b207a611be585
Romulo Tone <romulo.tone@etraveligroup.com>
2024-03-15 13:21:40 +0100

    AUTOCOMPLETION WAAAAAA

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,4 +1,5 @@
 return {
+  { "hrsh7th/cmp-nvim-lsp" },
   {
     "L3MON4D3/LuaSnip",
     dependencies = {
@@ -30,7 +31,7 @@ return {
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
-          --{ name = "nvim_lsp" },
+          { name = "nvim_lsp" },
           { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,34 +1,38 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "html", "cssls", "gopls", "jdtls", "gradle_ls", "jsonls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.html.setup({})
-			lspconfig.cssls.setup({})
-			lspconfig.gopls.setup({})
-			lspconfig.jdtls.setup({})
-			lspconfig.gradle_ls.setup({})
-			lspconfig.jsonls.setup({})
-			lspconfig.tsserver.setup({})
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "html", "cssls", "gopls", "jdtls", "gradle_ls", "jsonls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>g", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      lspconfig.lua_ls.setup({
+  capabilities
+      })
+      lspconfig.html.setup({capabilities = capabilities,})
+      lspconfig.cssls.setup({capabilities = capabilities,})
+      lspconfig.gopls.setup({capabilities = capabilities,})
+      lspconfig.jdtls.setup({capabilities = capabilities,})
+      lspconfig.gradle_ls.setup({capabilities = capabilities,})
+      lspconfig.jsonls.setup({capabilities = capabilities,})
+      lspconfig.tsserver.setup({capabilities = capabilities,})
+
+      vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>g", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
