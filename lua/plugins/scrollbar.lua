58563361cc20d51c4e2199233b2f27411c3080ed
Romulo Tone <romulo.tone@etraveligroup.com>
2024-03-18 19:02:18 +0100

    trying to have scroll and gitsigns

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -1,13 +1,6 @@
 return {
-	"petertriho/nvim-scrollbar",
-	dependencies = {
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
-	config = function()
-		require("scrollbar").setup()
-		require("scrollbar.handlers.gitsigns").setup()
-	end,
+  "petertriho/nvim-scrollbar",
+  config = function()
+    require("scrollbar").setup()
+  end,
 }
