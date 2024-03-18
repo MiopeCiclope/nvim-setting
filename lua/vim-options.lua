3e2592ac7416a834cb05f7ee0c08ab41d6844fbb
Romulo Tone <romulo.tone@etraveligroup.com>
2024-03-18 20:42:56 +0100

    color for html stuff

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set number")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set ignorecase")
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<C-a>', 'ggVG', {noremap = true})
