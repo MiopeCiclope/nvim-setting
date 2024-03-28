13380fb80dbe87f6263e7b06d7a7fc048cacc745
Romulo Tone <romulo.tone@etraveligroup.com>
2024-03-28 17:19:37 +0100

    tmux navigation

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,7 +6,15 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set ignorecase")
 vim.g.mapleader = " "
 
+vim.opt.swapfile = false
+
+-- Navigate vim panes
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+
 vim.keymap.set('n', '<C-a>', 'ggVG', {noremap = true})
 vim.keymap.set('n', '<C-m>', ':bnext<CR>', {noremap = true})
 vim.keymap.set('n', '<C-n>', ':bprevious<CR>', {noremap = true})
-vim.keymap.set('n', '<C-w>', ':bd<CR>', {noremap = true})
+vim.keymap.set('n', '<C-e>', ':bd<CR>!', {noremap = true})
