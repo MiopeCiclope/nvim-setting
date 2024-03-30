1ed811436f78ad67dd0505b471ed327bec97a80b
Romulo Tone <romulo.tone@etraveligroup.com>
2024-03-30 15:27:46 +0100

    telescope references

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<C-z>", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>r", builtin.lsp_references, {})
     end,
   },
   {
