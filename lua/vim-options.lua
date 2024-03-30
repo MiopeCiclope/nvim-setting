0e95832d8cc7401818b06267d20830266699d9a8
Romulo Tone <romulo.tone@etraveligroup.com>
2024-03-30 19:09:52 +0100

    adding keybinds for better motions

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,14 +9,18 @@ vim.g.mapleader = " "
 vim.opt.swapfile = false
 
 -- Navigate vim panes
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 
-vim.keymap.set('n', '<C-a>', 'ggVG', {noremap = true})
-vim.keymap.set('n', '<C-m>', ':bnext<CR>', {noremap = true})
-vim.keymap.set('n', '<C-n>', ':bprevious<CR>', {noremap = true})
-vim.keymap.set('n', '<C-e>', ':bd!<CR>', {noremap = true})
-vim.keymap.set('n', '<C-g>', ':DiffviewOpen<CR>', {noremap = true})
-vim.keymap.set('n', '<C-d>', ':DiffviewClose<CR>', {noremap = true})
+vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true })
+vim.keymap.set("n", "<C-m>", ":bnext<CR>", { noremap = true })
+vim.keymap.set("n", "<C-n>", ":bprevious<CR>", { noremap = true })
+vim.keymap.set("n", "<C-e>", ":bd!<CR>", { noremap = true })
+vim.keymap.set("n", "<C-g>", ":DiffviewOpen<CR>", { noremap = true })
+vim.keymap.set("n", "<C-d>", ":DiffviewClose<CR>", { noremap = true })
+vim.keymap.set("i", "<C-v>", "<C-o>$")
+vim.keymap.set({"n", "o"}, "t", "$")
+vim.keymap.set("i", "<C-c>", "<C-o>0")
+vim.keymap.set({"n", "o"}, "r", "g0")
