e4e52336b1ff3f156df308c1aa69f9e58298067b
Romulo Tone <romulo.tone@etraveligroup.com>
2024-03-31 16:38:00 +0200

    Adding diagnostics virtual line

--- lua/plugins/lsp-lines.lua
+++ lua/plugins/lsp-lines.lua
@@ -1,6 +1,7 @@
 return {
-  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  config = function()
-    require("lsp_lines").setup()
-  end,
+	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+	config = function()
+		require("lsp_lines").setup()
+		vim.keymap.set("n", "<leader>d", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+	end,
 }
