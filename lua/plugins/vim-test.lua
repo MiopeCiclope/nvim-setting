d9f13267b3c80e1c01a21c7c18cd436fde822505
Romulo Tone <romulo.tone@etraveligroup.com>
2024-03-31 17:30:11 +0200

    fixing test executable

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,14 +1,15 @@
 return {
-  "vim-test/vim-test",
-  dependencies = {
-    "preservim/vimux",
-  },
-  vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>"),
-  vim.keymap.set("n", "<leader>tf", ":TestFile<CR>"),
-  vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>"),
-  vim.keymap.set("n", "<leader>tl", ":TestLast<CR>"),
-  vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>"),
-  vim.cmd("let test#strategy = 'vimux'"),
-  vim.cmd("let g:VimuxOrientation = 'h'"),
-  vim.cmd("let g:VimuxHeight = '40'"),
+	"vim-test/vim-test",
+	dependencies = {
+		"preservim/vimux",
+	},
+	vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>"),
+	vim.keymap.set("n", "<leader>tf", ":TestFile<CR>"),
+	vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>"),
+	vim.keymap.set("n", "<leader>tl", ":TestLast<CR>"),
+	vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>"),
+	vim.cmd("let test#strategy = 'vimux'"),
+	vim.cmd("let g:VimuxOrientation = 'h'"),
+	vim.cmd("let g:VimuxHeight = '40'"),
+	vim.cmd("let g:test#javascript#jest#executable= 'npm run test'"),
 }
