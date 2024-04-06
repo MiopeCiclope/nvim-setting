31244510bba081a3265468653eb9d654593eb727
Romulo Tone <romulo.tone@cevt.se>
2024-04-06 17:10:47 +0200

    switching betweeen virtual lines and virtual text

--- lua/plugins/lsp-lines.lua
+++ lua/plugins/lsp-lines.lua
@@ -1,7 +1,6 @@
 return {
-	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-	config = function()
-		require("lsp_lines").setup()
-		vim.keymap.set("n", "<leader>d", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
-	end,
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  config = function()
+    require("lsp_lines").setup()
+  end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,9 +7,7 @@ vim.cmd("set ignorecase")
 vim.g.mapleader = " "
 
 vim.opt.swapfile = false
-vim.diagnostic.config({
-  virtual_text = false,
-})
+
 -- Navigate vim panes
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
@@ -22,8 +20,28 @@ vim.keymap.set("n", "<C-n>", ":bprevious<CR>", { noremap = true })
 vim.keymap.set("n", "<C-e>", ":bd!<CR>", { noremap = true })
 vim.keymap.set("n", "<C-g>", ":DiffviewOpen<CR>", { noremap = true })
 vim.keymap.set("n", "<C-d>", ":DiffviewClose<CR>", { noremap = true })
-vim.keymap.set("i", "<C-v>", "<C-o>$", {noremap = true})
-vim.keymap.set({ "n", "o", "v" }, "t", "$", {noremap = true})
-vim.keymap.set("i", "<C-c>", "<C-o>0", {noremap = true})
-vim.keymap.set({ "n", "o", "v" }, "r", "^", {noremap = true})
-vim.keymap.set({ "n", "o", "v" }, "w", "b", {noremap = true})
+vim.keymap.set("i", "<C-v>", "<C-o>$", { noremap = true })
+vim.keymap.set({ "n", "o", "v" }, "t", "$", { noremap = true })
+vim.keymap.set("i", "<C-c>", "<C-o>0", { noremap = true })
+vim.keymap.set({ "n", "o", "v" }, "r", "^", { noremap = true })
+vim.keymap.set({ "n", "o", "v" }, "w", "b", { noremap = true })
+
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = false
+})
+
+_G.toggle_virtual_text = function()
+  local current_value = vim.diagnostic.config().virtual_text
+  if current_value then
+    vim.diagnostic.config({ virtual_text = false })
+    vim.diagnostic.config({ virtual_lines = true })
+    print("Virtual line mode")
+  else
+    vim.diagnostic.config({ virtual_text = true })
+    vim.diagnostic.config( { virtual_lines = false })
+    print("Virtual text mode")
+  end
+end
+
+vim.keymap.set('n', '<Leader>d', '<cmd>lua toggle_virtual_text()<CR>', { noremap = true, silent = true })
