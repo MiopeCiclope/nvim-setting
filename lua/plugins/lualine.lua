dfb43dec645adb39028213c75e6f8a959eea9125
Romulo Tone <romulo.tone@etraveligroup.com>
2024-04-26 19:26:31 +0200

    having macro inside lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,38 +1,52 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "dracula",
-			},
-			tabline = {
-				lualine_a = { "buffers" },
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "filename" },
-				lualine_c = { "location" },
-				lualine_x = {
-					{
-						require("noice").api.status.command.get,
-						cond = require("noice").api.status.command.has,
-						color = { fg = "#ff9e64" },
-					},
-				},
-				lualine_y = {
-					{
-						require("noice").api.status.search.get,
-						cond = require("noice").api.status.search.has,
-						color = { fg = "#ff9e64" },
-					},
-				},
-				lualine_z = {
-					{
-						require("noice").api.status.message.get_hl,
-						cond = require("noice").api.status.message.has,
-					},
-				},
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    local function show_macro_recording()
+      local recording_register = vim.fn.reg_recording()
+      if recording_register == "" then
+        return ""
+      else
+        return "Recording @" .. recording_register
+      end
+    end
+
+    require("lualine").setup({
+      options = {
+        theme = "dracula",
+      },
+      tabline = {
+        lualine_a = { "buffers" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "filename", "location" },
+        lualine_c = {
+          {
+            "macro-recording",
+            fmt = show_macro_recording,
+          },
+        },
+        lualine_x = {
+          {
+            require("noice").api.status.command.get,
+            cond = require("noice").api.status.command.has,
+            color = { fg = "white" },
+          },
+        },
+        lualine_y = {
+          {
+            require("noice").api.status.search.get,
+            cond = require("noice").api.status.search.has,
+            color = { fg = "white" },
+          },
+        },
+        lualine_z = {
+          {
+            require("noice").api.status.message.get_hl,
+            cond = require("noice").api.status.message.has,
+          },
+        },
+      },
+    })
+  end,
 }
