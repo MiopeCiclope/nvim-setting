a5b5d5de670001ec205fb93d10ed69f32e3ec4f8
Romulo Tone <romulo.tone@etraveligroup.com>
2024-04-26 19:11:31 +0200

    adding voice

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,38 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "dracula",
-      },
-      tabline = {
-        lualine_a = { "buffers" },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			tabline = {
+				lualine_a = { "buffers" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "filename" },
+				lualine_c = { "location" },
+				lualine_x = {
+					{
+						require("noice").api.status.command.get,
+						cond = require("noice").api.status.command.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+				lualine_y = {
+					{
+						require("noice").api.status.search.get,
+						cond = require("noice").api.status.search.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+				lualine_z = {
+					{
+						require("noice").api.status.message.get_hl,
+						cond = require("noice").api.status.message.has,
+					},
+				},
+			},
+		})
+	end,
 }
