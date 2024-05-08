f2e40b8a6caa765ddb7f2a8beb88793bb747f97b
Romulo Tone <romulo.tone@etraveligroup.com>
2024-05-08 15:04:46 +0200

    git blame highlight

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,8 +1,9 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup({
-      current_line_blame = false,
-    })
-  end,
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			current_line_blame = false,
+		})
+		vim.cmd([[highlight GitSignsCurrentLineBlame guifg=#ffffff gui=bold]])
+	end,
 }
