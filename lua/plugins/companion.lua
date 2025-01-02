09c4213937bc7269b31c0f065c23a37bb2b1714f
Romulo Tone <romulo.tone@etraveligroup.com>
2025-01-02 13:23:46 +0100

    testing companion

---
+++ lua/plugins/companion.lua
@@ -0,0 +1,43 @@
+local DEFAULT_MODEL = "codestral"
+
+return {
+  "olimorris/codecompanion.nvim",
+  opts = {
+    adapters = {
+      ollama = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          schema = {
+            model = {
+              default = DEFAULT_MODEL,
+            },
+          },
+        })
+      end,
+    },
+    strategies = {
+      chat = {
+        adapter = "ollama",
+        model = DEFAULT_MODEL, -- Use the default model here
+        keymaps = {
+          send = {
+            modes = {
+              n = "<CR>",
+              i = "<C-CR>",
+            },
+            index = 1,
+            callback = "keymaps.send",
+            description = "Send",
+          },
+        },
+      },
+      inline = {
+        adapter = "ollama",
+        model = DEFAULT_MODEL, -- Use the default model here
+      },
+      agent = {
+        adapter = "ollama",
+        model = DEFAULT_MODEL, -- Use the default model here
+      },
+    },
+  },
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -53,16 +53,17 @@ vim.keymap.set("n", '<leader>"', ":split<CR>", { noremap = true, silent = true }
 vim.keymap.set("n", "<Leader>l", "<Cmd>noh<CR>", { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap("n", "<C-x>", ":Explore<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>:", ":AvanteToggle<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<leader>:", ":AvanteToggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>:", ":CodeCompanionChat<CR>", { noremap = true, silent = true })
 
 vim.diagnostic.config({
-	virtual_text = true,
-	virtual_lines = false,
+  virtual_text = true,
+  virtual_lines = false,
 })
 
 _G.toggle_diagnostics = function()
-	local currentValue = vim.diagnostic.config().virtual_text
-	vim.diagnostic.config({ virtual_text = not currentValue })
+  local currentValue = vim.diagnostic.config().virtual_text
+  vim.diagnostic.config({ virtual_text = not currentValue })
 end
 
 vim.keymap.set("n", "<Leader>d", "<cmd>lua toggle_diagnostics()<CR>", { noremap = true, silent = true })
@@ -70,30 +71,30 @@ vim.keymap.set("x", "/", ":<C-u>/\\%V", { noremap = true, silent = true })
 
 -- makes keymaps with less words
 function _G.map(mode, keys, command)
-	vim.api.nvim_set_keymap(mode, keys, command, { noremap = true })
+  vim.api.nvim_set_keymap(mode, keys, command, { noremap = true })
 end
 
 --#region autopair replacer
 -- create keymaps for automatically close brankets
 local function makeTagKeymap(char, completion)
-	local command = char .. completion .. "<Esc>ha"
-	map("i", char, command)
+  local command = char .. completion .. "<Esc>ha"
+  map("i", char, command)
 end
 
 local function setupTagCompletion(mapping_table)
-	for char, completion in pairs(mapping_table) do
-		makeTagKeymap(char, completion)
-	end
+  for char, completion in pairs(mapping_table) do
+    makeTagKeymap(char, completion)
+  end
 end
 
 -- Define your table of mappings
 local mappings = {
-	["{"] = "}",
-	["["] = "]",
-	["("] = ")",
-	['"'] = '"',
-	["´"] = "´",
-	["`"] = "`",
+  ["{"] = "}",
+  ["["] = "]",
+  ["("] = ")",
+  ['"'] = '"',
+  ["´"] = "´",
+  ["`"] = "`",
 }
 
 setupTagCompletion(mappings)
@@ -101,31 +102,31 @@ setupTagCompletion(mappings)
 
 -- Function to open diagnostics in a new buffer
 function OpenDiagnosticsInNewBuffer()
-	local diagnostics = vim.diagnostic.get()
-	if vim.tbl_isempty(diagnostics) then
-		print("No diagnostics to display")
-		return
-	end
+  local diagnostics = vim.diagnostic.get()
+  if vim.tbl_isempty(diagnostics) then
+    print("No diagnostics to display")
+    return
+  end
 
-	-- Create a new buffer
-	vim.cmd("new")
-	local buf = vim.api.nvim_get_current_buf()
+  -- Create a new buffer
+  vim.cmd("new")
+  local buf = vim.api.nvim_get_current_buf()
 
-	-- Set buffer options
-	vim.bo[buf].buftype = "nofile"
-	vim.bo[buf].bufhidden = "wipe"
-	vim.bo[buf].swapfile = false
+  -- Set buffer options
+  vim.bo[buf].buftype = "nofile"
+  vim.bo[buf].bufhidden = "wipe"
+  vim.bo[buf].swapfile = false
 
-	-- Populate buffer with diagnostics
-	local lines = {}
-	for _, diagnostic in ipairs(diagnostics) do
-		local clean_message = diagnostic.message:gsub("\n", " ")
-		local line =
-			string.format("%s:%d:%d: %s", diagnostic.source, diagnostic.lnum + 1, diagnostic.col + 1, clean_message)
-		table.insert(lines, line)
-	end
+  -- Populate buffer with diagnostics
+  local lines = {}
+  for _, diagnostic in ipairs(diagnostics) do
+    local clean_message = diagnostic.message:gsub("\n", " ")
+    local line =
+        string.format("%s:%d:%d: %s", diagnostic.source, diagnostic.lnum + 1, diagnostic.col + 1, clean_message)
+    table.insert(lines, line)
+  end
 
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
 end
 
 -- Create a command to open diagnostics in a new buffer
@@ -139,21 +140,21 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-N>", { noremap = true, silent = true })
 
 -- allow changes on terminal inside vim
 vim.api.nvim_create_autocmd("TermOpen", {
-	pattern = "*",
-	callback = function()
-		vim.opt_local.modifiable = true
-	end,
+  pattern = "*",
+  callback = function()
+    vim.opt_local.modifiable = true
+  end,
 })
 
 local function augroup(name)
-	return vim.api.nvim_create_augroup("my_lazyvim_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup("my_lazyvim_" .. name, { clear = true })
 end
 
 -- Set filetype for .hurl files
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	group = augroup("env_filetype"),
-	pattern = { "*.http" },
-	callback = function()
-		vim.opt_local.filetype = "sh"
-	end,
+  group = augroup("env_filetype"),
+  pattern = { "*.http" },
+  callback = function()
+    vim.opt_local.filetype = "sh"
+  end,
 })
