dd613fc74ed787ae05cc1b606b859eaf16adc82f
Romulo Tone <romulo.tone@etraveligroup.com>
2025-01-30 14:22:00 +0100

    Done fixing blink

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,27 +1,74 @@
 return {
-	"saghen/blink.cmp",
-	version = "*",
-	opts = {
-		keymap = {
-			preset = "default",
-			["<C-space>"] = { "accept" },
-		},
-
-		appearance = {
-			use_nvim_cmp_as_default = true, -- Ensure blink.cmp uses nvim-cmp
-			nerd_font_variant = "mono",
-		},
-
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-			cmdline = {},
-		},
-		completion = {
-			menu = { border = "single" },
-			documentation = { window = { border = "single" } },
-		},
-		signature = { window = { border = "single" } },
-	},
-
-	opts_extend = { "sources.default" },
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    build = "make install_jsregexp", -- if you're on windows remove this line
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+      end,
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    opts = {
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        cmdline = function()
+          local type = vim.fn.getcmdtype()
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          if type == ":" or type == "@" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
+      },
+      completion = {
+        documentation = {
+          window = {
+            border = "single",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:CursorLine,Search:None",
+          },
+        },
+        menu = {
+          border = "single",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:CursorLine,Search:None",
+          draw = {
+            columns = function(ctx)
+              if ctx.mode == "cmdline" then
+                return { { "label" } }
+              else
+                return { { "kind_icon" }, { "source_name" }, { "label", "label_description", gap = 1 } }
+              end
+            end,
+          },
+        },
+      },
+      keymap = {
+        ["<TAB>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+      },
+      signature = {
+        enabled = true,
+        window = {
+          border = "single",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:CursorLine,Search:None",
+        },
+      },
+    },
+  },
 }
