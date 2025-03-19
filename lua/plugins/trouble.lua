8a8cb515b53c10f61a0a97f9f6c1340a14a38676
Romulo Tone <romulo.tone@etraveligroup.com>
2025-03-19 11:26:55 +0100

    try copy files

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,25 +1,17 @@
 return {
-  "folke/trouble.nvim",
-  cmd = "Trouble",
-  keys = {
-    {
-      "<leader>x",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
-      "<leader>r",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-  },
-  config = function()
-    require("trouble").setup({
-      win = {
-        size = {
-          width = 80,
-        },
-      },
-    })
-  end,
+	"folke/trouble.nvim",
+	opts = {},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>x",
+			"<cmd>Trouble diagnostics toggle focus=true filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>r",
+			"<cmd>Trouble lsp_references toggle focus=true win.position=right win.size.width=80<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+	},
 }
