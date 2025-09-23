24240a14d4326c19546bfb09b4f0a053a65725c7
Romulo Tone <romulo.tone@etraveligroup.com>
2025-09-23 20:52:41 +0200

    adding command to create quickfix list from fzf

--- lua/fzf.lua
+++ lua/fzf.lua
@@ -2,7 +2,7 @@ local utils = require("utils")
 local M = {}
 
 M.PREVIEW_COMMAND = " --preview 'bat --color=always --style=numbers --line-range :500 {}'"
-M.FZF_COMMAND = " | fzf --ansi --multi --height 100% --border"
+M.FZF_COMMAND = " | fzf --ansi --multi --height 100% --border --bind 'ctrl-q:select-all'"
 
 -- Main fzf function for git files
 function M.git_files()
@@ -59,21 +59,25 @@ function M.grep_search()
 		.. M.FZF_COMMAND
 		.. " --delimiter=':' --preview 'bat --style=numbers --color=always --highlight-line {2} --line-range {2}:+20 {1}'"
 
-	M.fzf_command(fzf_cmd, function(selected)
-		local parts = vim.split(selected, ":", { plain = true })
+	M.fzf_command(fzf_cmd)
+end
 
-		local filename = parts[1]
-		local line_number = parts[2]
+function M.default_callback(selected_list)
+	if #selected_list > 2 then
+		vim.fn.setqflist(selected_list, "r")
+		vim.cmd("copen")
+	end
 
-		utils.open_file(filename, line_number)
-	end)
+	if #selected_list == 1 then
+		utils.open_file(selected_list[1].filename)
+	end
 end
 
 function M.fzf_command(cmd, callback)
 	local temp_file = utils.get_temp_file("_fzf")
 	local win = utils.create_float_window()
 
-	local callback_function = callback or utils.open_file
+	local callback_function = callback or M.default_callback
 	local fzf_cmd = cmd .. " > " .. temp_file
 
 	vim.fn.jobstart(fzf_cmd, {
@@ -92,7 +96,19 @@ function M.fzf_command(cmd, callback)
 						file:close()
 
 						if selected ~= "" then
-							return callback_function(selected)
+							local qflist = {}
+
+							for line in string.gmatch(selected, "[^\n]+") do
+								local parts = vim.split(line, ":", { plain = true })
+
+								local filename = parts[1]
+								local line_number = parts[2] or 1
+								local text = parts[3] or ""
+
+								table.insert(qflist, { filename = filename, lnum = line_number, text = text })
+							end
+
+							return callback_function(qflist)
 						end
 					end
 				end
