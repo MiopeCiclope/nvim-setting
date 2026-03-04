550b800da2f591163e3e4ad1554281adf874d212
Romulo Tone <romulo.tone@etraveligroup.com>
2026-03-04 09:29:13 +0100

    working

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -14,7 +14,8 @@ return {
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "java",
 			callback = function()
-				local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+				local root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" })
+				local project_name = root_dir and vim.fn.fnamemodify(root_dir, ":t") or vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
 				local workspace_dir = "/Users/romulotone/.local/share/jdtls-workspace/" .. project_name
 
 				local config = {
@@ -22,11 +23,13 @@ return {
 						"/Users/romulotone/.jenv/versions/21.0.4/bin/java",
 
 						-- JVM flags MUST come before -jar or they are passed to the launcher as args, not to the JVM
-						"-Xms2g",
-						"-Xmx12g",
+						"-Xms4g",
+						"-Xmx20g",
 						"-XX:ReservedCodeCacheSize=512m",
 						"-XX:CICompilerCount=" .. ci_count,
 						"-XX:+UseG1GC",
+						"-XX:G1HeapRegionSize=32m",
+						"-XX:InitiatingHeapOccupancyPercent=25",
 						"-XX:GCTimeRatio=4",
 						"-XX:AdaptiveSizePolicyWeight=90",
 						"-XX:+UseStringDeduplication",
@@ -50,7 +53,7 @@ return {
 						workspace_dir,
 					},
 
-					root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
+					root_dir = root_dir,
 
 					flags = {
 						debounce_text_changes = 500,
@@ -92,6 +95,14 @@ return {
 				}
 
 				require("jdtls").start_or_attach(config)
+
+				vim.api.nvim_buf_create_user_command(0, "JavaRename", function()
+					vim.lsp.buf.rename()
+				end, { desc = "Rename symbol across project" })
+
+				vim.api.nvim_buf_create_user_command(0, "JavaImplementations", function()
+					vim.lsp.buf.implementation()
+				end, { desc = "Show all implementations of symbol" })
 			end,
 		})
 	end,
